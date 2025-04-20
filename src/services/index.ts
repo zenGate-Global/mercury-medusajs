9aebfc49714f952e9104185cf4eb7c42bd15be4e
Adam Dean <adam@crypto2099.io>
2025-04-20 06:13:22 -0700

    Remove some of the legacy stuff

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import CardanoMercuryService from "./service"
-import {
-    ModuleProvider,
-    Modules
-} from "@medusajs/utils"
-
-export default ModuleProvider(Modules.PAYMENT, {
-    services: [CardanoMercuryService],
-})
